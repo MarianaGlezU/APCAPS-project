1d4d79cc4730433e3d8b6d3db59715de4dce589d
MarianaGlezU <mglzu24@gmail.com>
2024-05-27 22:08:35 +0100

    Create dependencies.R

---
+++ dependencies.R
@@ -0,0 +1,14 @@
+packages <- c(
+  "skimr",
+  "janitor",
+  "survival",
+  "ranger",
+  "ggplot2",
+  "dplyr",
+  "tidyverse",
+  "haven",
+  "sjPlot",
+  "gtsummary"
+)
+
+install.packages(packages)
